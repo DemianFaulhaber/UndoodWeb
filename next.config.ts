dc98ed7f15c93013572b23fcf4988692f41410c6
Demian Faulhaber <demiansilver22@gmail.com>
2025-11-14 20:47:32 -0300

    fix on config

--- next.config.ts
+++ next.config.ts
@@ -6,14 +6,31 @@ const nextConfig: NextConfig = {
     },
     // Ignorar archivos que no son páginas/componentes de Next.js
     pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-    webpack: (config) => {
+    webpack: (config, { isServer }) => {
         // Excluir archivos HTML de la carpeta emails del bundle
         config.module.rules.push({
             test: /\.html$/,
             exclude: /node_modules/,
             type: 'asset/source',
         });
+        
+        // Ignorar carpeta send_message durante el build
+        config.plugins.push(
+            new (require('webpack').IgnorePlugin)({
+                resourceRegExp: /^\.\/send_message$/,
+                contextRegExp: /app\/api$/,
+            })
+        );
+        
         return config;
+    },
+    // Excluir rutas de API específicas del build
+    experimental: {
+        outputFileTracingExcludes: {
+            '*': [
+                'app/api/send_message/**/*',
+            ],
+        },
     },
 };
 
