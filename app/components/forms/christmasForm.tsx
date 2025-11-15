352c89104838298fc9382f8dc1fc46711d193ea2
Demian Faulhaber <demiansilver22@gmail.com>
2025-11-14 21:09:41 -0300

    sos re puto next

--- app/components/forms/christmasForm.tsx
+++ app/components/forms/christmasForm.tsx
@@ -110,8 +110,7 @@ export function ChristmasForm() {
             // Formatear children
             const formattedChildren = childrenData.map((child) => ({
                 id: child.idchildren,
-                label: `${child.name}, 
-                ${child.age !== 99 ? (child.age >= 1 ? child.age + ' años,' : decimalToMonths(child.age) + ' meses,') : ''} de ${child.house}`,
+                label: `${child.name}, ${child.age !== 99 ? (child.age >= 1 ? child.age + ' años,' : decimalToMonths(child.age) + ' meses,') : ''} de ${child.house}`,
                 available: child.available
             }));
             
